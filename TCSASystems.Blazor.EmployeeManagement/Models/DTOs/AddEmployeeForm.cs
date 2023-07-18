944656eab5d60a58edd03c6dcab84b14a631b2c0
Pablo <thecsharpacademy@gmail.com>
2023-07-19 06:36:07 +1000

    Done, I think?

---
+++ TCSASystems.Blazor.EmployeeManagement/Models/DTOs/AddEmployeeForm.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TCSASystems.Blazor.EmployeeManagement.Models.DTOs;
+
+public class AddEmployeeForm
+{
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public decimal Salary { get; set; }
+
+    [Required]
+    public EmployeeType Type { get; set; }
+
+    [Required]
+    public Position Position { get; set; }
+
+    [Required]
+    public string ImgUrl { get; set; }
+}
--- TCSASystems.Blazor.EmployeeManagement/Services/EmployeeService.cs
+++ TCSASystems.Blazor.EmployeeManagement/Services/EmployeeService.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TCSASystems.Blazor.EmployeeManagement.Data;
+using TCSASystems.Blazor.EmployeeManagement.Models;
+using TCSASystems.Blazor.EmployeeManagement.Models.DTOs;
 using TCSASystems.Blazor.EmployeeManagement.Models.Responses;
 
 namespace TCSASystems.Blazor.EmployeeManagement.Services;
@@ -7,6 +9,7 @@ namespace TCSASystems.Blazor.EmployeeManagement.Services;
 public interface IEmployeeService
 {
     Task<GetEmployeesResponse> GetEmployees();
+    Task<BaseResponse> AddEmployee(AddEmployeeForm form);
 }
 
 public class EmployeeService: IEmployeeService
@@ -35,6 +38,44 @@ public class EmployeeService: IEmployeeService
             response.StatusCode = 500;
             response.Message = "Error retrieving employees: " + ex.Message;
             response.Employees = null;
+        }
+
+        return response;
+    }
+    public async Task<BaseResponse> AddEmployee(AddEmployeeForm form)
+    {
+        var response = new BaseResponse();
+        try
+        {
+            using (var context = _factory.CreateDbContext())
+            {
+                context.Add(new Employee
+                {
+                    Name = form.Name,
+                    Position = form.Position,
+                    Salary = form.Salary,
+                    Type = form.Type,
+                    ImgUrl = form.ImgUrl
+                });
+
+                var result = await context.SaveChangesAsync();
+
+                if (result == 1)
+                {
+                    response.StatusCode = 200;
+                    response.Message = "Employee added successfully";
+                }
+                else
+                {
+                    response.StatusCode = 400;
+                    response.Message = "Error occurred while adding the employee.";
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            response.StatusCode = 500;
+            response.Message = "Error adding employee: " + ex.Message;
         }
 
         return response;
